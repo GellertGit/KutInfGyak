4a3e15c3262831a5e6a5e8382269886bdb227509
GellertGit <gasparjeno9@gmail.com>
2017-05-24 17:34:12 +0200

    Add files via upload

---
+++ KutInfGyak/build/app/CMakeFiles/Application.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Application.dir/src/app.c.o"
+  "Application.pdb"
+  "Application"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ KutInfGyak/build/phys/CMakeFiles/phys.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/phys.dir/src/phys.c.o"
+  "libphys.pdb"
+  "libphys.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/phys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
